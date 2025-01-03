5338be31223e61a4ebe6708ce05281d0e49bbcb0
Galina Bobrik <galagrinko@gmail.com>
2025-01-03 18:08:49 +0400

    feat: add filter by author, resetFilter

--- frontend/src/components/BookList/BookList.jsx
+++ frontend/src/components/BookList/BookList.jsx
@@ -2,11 +2,18 @@ import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 import { BsBookmarkHeart, BsBookmarkHeartFill } from "react-icons/bs";
 import { deleteBook, toggleFavorite } from "../../redux/books/actionCreators";
+import {
+    selectTitleFilter,
+    selectAuthorFilter,
+} from "../../redux/slices/filterSlice";
 
 import "./BookList.css";
 
 const BookList = () => {
     const books = useSelector((state) => state.books);
+    const titleFilter = useSelector(selectTitleFilter);
+    const authorFilter = useSelector(selectAuthorFilter);
+
     const dispatch = useDispatch();
 
     const handleDeleteBook = (id) => {
@@ -16,6 +23,19 @@ const BookList = () => {
     const handleToggleFavorite = (id) => {
         dispatch(toggleFavorite(id));
     };
+
+    const filteredBooks = books.filter((book) => {
+        const matchesTitle = book.title
+            .toLowerCase()
+            .includes(titleFilter.toLowerCase());
+
+        const matchesAuthor = book.author
+            .toLowerCase()
+            .includes(authorFilter.toLowerCase());
+
+        return matchesTitle && matchesAuthor;
+    });
+
     return (
         <div className="app-block book-list">
             <h2>Book List</h2>
@@ -23,7 +43,7 @@ const BookList = () => {
                 <p>No books available</p>
             ) : (
                 <ul>
-                    {books.map((book, index) => (
+                    {filteredBooks.map((book, index) => (
                         <li key={book.id}>
                             <div className="book-info">
                                 {++index}. {book.title} by{" "}
--- frontend/src/components/Filter/Filter.jsx
+++ frontend/src/components/Filter/Filter.jsx
@@ -1,27 +1,52 @@
 import { useDispatch, useSelector } from "react-redux";
 import {
     setTitleFilter,
+    setAuthorFilter,
     selectTitleFilter,
+    selectAuthorFilter,
+    resetFilters,
 } from "../../redux/slices/filterSlice";
 import "./Filter.css";
 
 const Filter = () => {
     const dispatch = useDispatch();
     const titleFilter = useSelector(selectTitleFilter);
+    const authorFilter = useSelector(selectAuthorFilter);
 
     const handleTitleFilterChange = (e) => {
         dispatch(setTitleFilter(e.target.value));
     };
 
+    const handelResetfilters = () => {
+        dispatch(resetFilters());
+    };
+
+    const handleAuthorFilterChange = (e) => {
+        dispatch(setAuthorFilter(e.target.value));
+    };
+
     return (
         <div className="app-block filter">
-            <div className="filter-group">
-                <input
-                    type="text"
-                    placeholder="filter by title..."
-                    onChange={handleTitleFilterChange}
-                    value={titleFilter}
-                />
+            <div className="filter-row">
+                <div className="filter-group">
+                    <input
+                        type="text"
+                        placeholder="filter by title..."
+                        onChange={handleTitleFilterChange}
+                        value={titleFilter}
+                    />
+                </div>
+                <div className="filter-group">
+                    <input
+                        type="text"
+                        placeholder="filter by author..."
+                        onChange={handleAuthorFilterChange}
+                        value={authorFilter}
+                    />
+                </div>
+                <button type="button" onClick={handelResetfilters}>
+                    Reset filters
+                </button>
             </div>
         </div>
     );
