2092bd66139e5f647c13b8ff5e73af03551f194c
Galina Bobrik <galagrinko@gmail.com>
2025-01-03 13:35:47 +0400

    feat: add filterSlice and filter by title

--- frontend/src/components/BookForm/BookForm.jsx
+++ frontend/src/components/BookForm/BookForm.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { useDispatch } from "react-redux";
-
 import { addBook } from "../../redux/books/actionCreators";
 import booksData from "../../data/books.json";
 import createBookWithId from "../../utils/createBookWithId";
