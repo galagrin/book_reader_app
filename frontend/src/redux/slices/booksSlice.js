a13d660007aafee8f53768502618e6a166734c6f
Galina Bobrik <galagrinko@gmail.com>
2025-01-05 18:41:58 +0400

    feat: add fetchbook.fullfilled

--- frontend/src/redux/slices/booksSlice.js
+++ frontend/src/redux/slices/booksSlice.js
@@ -1,12 +1,24 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 import createBookWithId from '../../utils/createBookWithId';
 
 const initialState = [];
 
+export const fetchBook = createAsyncThunk(
+    // назвние типа действия
+    'books/fetchbook',
+    // асинхронная функция
+    async () => {
+        const res = await axios.get('http://localhost:4000/random-book');
+        console.log(res.data);
+        return res.data;
+    },
+);
+
 const booksSlice = createSlice({
     name: 'books',
     initialState: initialState,
+
     reducers: {
         addBook: (state, action) => {
             return [...state, action.payload];
@@ -18,20 +30,17 @@ const booksSlice = createSlice({
             return state.map((book) => (book.id === action.payload ? { ...book, isFavorite: !book.isFavorite } : book));
         },
     },
+
+    extraReducers: (builder) => {
+        builder.addCase(fetchBook.fulfilled, (state, action) => {
+            if (action.payload.title && action.payload.author) {
+                state.push(createBookWithId(action.payload, 'API'));
+            }
+        });
+    },
 });
 
 export const { addBook, deleteBook, toggleFavorite } = booksSlice.actions;
-
-export const thunkFunction = async (dispatch, getState) => {
-    try {
-        const res = await axios.get('http://localhost:4000/random-book');
-        if (res.data && res.data.title && res.data.author) {
-            dispatch(addBook(createBookWithId(res.data, 'API')));
-        }
-    } catch (error) {
-        console.log('Error fetching random book', error);
-    }
-};
 
 export const selectBooks = (state) => state.books;
 
